081afee4deb829b8c1346fa9dc8b0f87101fd47d
nohawaheed <nohawaheed.1991@gmail.com>
2024-11-19 21:59:18 +0200

    add auth endpoints

---
+++ projects/ngx-auth-api/src/lib/enums/ngx-auth-api.endpoint.ts
@@ -0,0 +1,16 @@
+export class AuthEndpoint {
+  static LOGIN = 'https://exam.elevateegy.com/api/v1/auth/signin';
+  static REGISTER = 'https://exam.elevateegy.com/api/v1/auth/signup';
+  static CHANGE_PASSWORD =
+    'https://exam.elevateegy.com/api/v1/auth/changePassword';
+  static DELETE_ACCOUNT = 'https://exam.elevateegy.com/api/v1/auth/deleteMe';
+  static EDIT_PROFILE = 'https://exam.elevateegy.com/api/v1/auth/editProfile';
+  static LOG0UT = 'https://exam.elevateegy.com/api/v1/auth/logout';
+  static USER_INFO = 'https://exam.elevateegy.com/api/v1/auth/profileData';
+  static FORGET_PASSWORD =
+    'https://exam.elevateegy.com/api/v1/auth/forgotPassword';
+  static VERIFY_CODE =
+    'https://exam.elevateegy.com/api/v1/auth/verifyResetCode';
+  static RESET_PASSWORD =
+    'https://exam.elevateegy.com/api/v1/auth/resetPassword';
+}
