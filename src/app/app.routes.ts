f43a615c19d72fbb973e6fafb143348865767f64
nohawaheed <nohawaheed.1991@gmail.com>
2024-11-25 18:50:29 +0200

    config project routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', 
+    loadComponent: ()=> import('../core/layout/auth-layout/auth-layout.component').then(
+      (c) => c.AuthLayoutComponent),
+      children: [
+        { path: '', redirectTo: 'login', pathMatch: 'full' },
+        {
+          path: 'login',
+          loadComponent: () =>
+            import('../core/pages/login/login.component').then(
+              (c) => c.LoginComponent
+            ),
+        },
+        {
+          path: 'register',
+          loadComponent: () =>
+            import('../core/pages/register/register.component').then(
+              (c) => c.RegisterComponent
+            ),
+        },
+      ] 
+  },
+
+];
