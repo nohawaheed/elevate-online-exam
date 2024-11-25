01386569386629069e7fdb078e830438cb588e19
nohawaheed <nohawaheed.1991@gmail.com>
2024-11-25 18:53:18 +0200

    add button component

---
+++ src/shared/components/ui/button/button.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, Input, InputSignal, output } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [ButtonModule, CommonModule ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() label : string = 'Add';
+  @Input() type : string = 'button';
+  @Input() padding : string = '1rem 0rem';
+  @Input() width : string = '100%';
+  disabled : InputSignal<boolean>  = input.required<boolean>();
+  buttonClicked = output<boolean>();
+
+  onButtonClicked(event: MouseEvent): void {
+    this.buttonClicked.emit(true);
+  }
+}
