a79ff023d6d4ad25702945f6fb4439e11f6be0da
nohawaheed <nohawaheed.1991@gmail.com>
2024-11-25 18:58:25 +0200

    add auth library interface

---
+++ projects/ngx-auth-api/src/lib/interfaces/auth.interfaces.ts
@@ -0,0 +1,35 @@
+export interface RegisterRequest {
+    username:string;
+    firstName:string;
+    lastName:string;
+    email: string;
+    password: string;
+    rePassword: string;
+    phone: string;
+}
+export interface RegisterResponse {
+    message: string;
+    token: string;
+    user: {
+        username: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+        phone: string;
+        role: string;
+        isVerified: boolean;
+        _id: string;
+        createdAt: string;
+    }  
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface AuthResponse {
+    message: string;
+    token: string;
+    userEmail: string;
+}
