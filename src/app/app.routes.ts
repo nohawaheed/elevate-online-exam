260f550547be22161870b3ada6c3e10f7f0a78ed
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-06 17:50:20 +0200

    update route paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,28 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', 
-    loadComponent: ()=> import('../core/layout/auth-layout/auth-layout.component').then(
+    loadComponent: ()=> import('./core/layout/auth-layout/auth-layout.component').then(
       (c) => c.AuthLayoutComponent),
       children: [
         { path: '', redirectTo: 'login', pathMatch: 'full' },
         {
           path: 'login',
           loadComponent: () =>
-            import('../core/pages/login/login.component').then(
+            import('./core/pages/login/login.component').then(
               (c) => c.LoginComponent
             ),
         },
         {
           path: 'register',
           loadComponent: () =>
-            import('../core/pages/register/register.component').then(
+            import('./core/pages/register/register.component').then(
               (c) => c.RegisterComponent
             ),
         },
         {
           path: 'forgot-password',
           loadComponent: () => 
-            import('../core/pages/forgot-password/forgot-password.component').then(
+            import('./core/pages/forgot-password/forgot-password.component').then(
               (c) => c.ForgotPasswordComponent
           )
         },
@@ -32,7 +32,7 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('../feature/pages/home/home.component').then(
+      import('./feature/pages/home/home.component').then(
         (c) => c.HomeComponent
       ),
   }
