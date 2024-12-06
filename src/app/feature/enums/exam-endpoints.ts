9f484fe8e39757952828f5dbade8e38b56887beb
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-06 17:49:53 +0200

    move core, feature, and shared folders inside app folder

---
+++ src/app/feature/enums/exam-endpoints.ts
@@ -0,0 +1,6 @@
+export class ExamEndpoint {
+    static ADD_OR_GET_EXAMS = '/api/v1/exams';
+    static GET_EXAMS_ON_SUBJECT ='/api/v1/exams?subject=';
+    static GET_EXAM_BY_ID = '/api/v1/exams/';
+  }
+  
