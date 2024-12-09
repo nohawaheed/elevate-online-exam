d42c7c2c143315772421afb62664df1f1c4bf7cb
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-09 20:06:55 +0200

    add exame service

--- src/app/feature/services/exam.service.ts
+++ src/app/feature/services/exam.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ExamEndpoint } from '../enums/exam-endpoints';
+import { map, Observable } from 'rxjs';
+import { AllSubjects, SubjectAdapted } from '../interfaces/subject';
+import { SubjectAdapter } from '../adapter/subject.adapter';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExamService {
+  constructor(
+    private _httpClient: HttpClient,
+    private _subjectAdapter: SubjectAdapter
+  ) {}
 
-  constructor() { }
+  getSubjectsWithLimit(
+    page: number,
+    limit: number
+  ): Observable<SubjectAdapted> {
+    return this._httpClient
+      .get(`${ExamEndpoint.GET_ALL_SUBJECTS}page=${page}&limit=${limit}`)
+      .pipe(
+        map((res) => this._subjectAdapter.adaptSubject(res as AllSubjects))
+      );
+  }
 }
