a128b1d86bf61dd9060e69e25f37a5c30ff747ca
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-11 22:53:45 +0200

    add exam questions api request

--- src/app/feature/services/exam.service.ts
+++ src/app/feature/services/exam.service.ts
@@ -4,7 +4,7 @@ import { ExamEndpoint } from '../enums/exam-endpoints';
 import { map, Observable } from 'rxjs';
 import { AllSubjects, SubjectAdapted } from '../interfaces/subject';
 import { SubjectAdapter } from '../adapter/subject.adapter';
-import { AllExams, ExamAdapted } from '../interfaces/exams';
+import { AllExams, ExamAdapted, ExamQuestions } from '../interfaces/exams';
 import { ExamAdapter } from '../adapter/exam.adapter';
 
 @Injectable({
@@ -36,5 +36,11 @@ export class ExamService {
         `${ExamEndpoint.GET_EXAMS_ON_SUBJECT}${subjectId}&page=${page}&limit=${limit}`
       )
       .pipe(map((res) => this._examAdapter.adapt(res as AllExams)));
+  }
+
+  getQuestionsOnExam(examId: string): Observable<ExamQuestions> {
+    return this._httpClient
+      .get(`${ExamEndpoint.GET_ALL_EXAM_QUESTIONS}${examId}`)
+      .pipe(map((res) => res as ExamQuestions));
   }
 }
