3eae0d86047db64c68938c99e7c3d1d7b5e96115
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-13 16:42:52 +0200

    update home component

--- src/app/feature/pages/home/home.component.ts
+++ src/app/feature/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeNavbarComponent } from '../home-navbar/home-navbar.component';
+import { QuizesComponent } from '../quizes/quizes.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeNavbarComponent, QuizesComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
