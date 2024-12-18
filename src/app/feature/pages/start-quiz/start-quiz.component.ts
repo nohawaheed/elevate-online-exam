5834db679ab14d56007038a9cd33d2a94e8a1497
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-18 23:45:28 +0200

    show exam dialog on exam start

--- src/app/feature/pages/start-quiz/start-quiz.component.ts
+++ src/app/feature/pages/start-quiz/start-quiz.component.ts
@@ -1,16 +1,30 @@
 import { Component, Input, OnInit, signal } from '@angular/core';
 import { DataViewModule } from 'primeng/dataview';
-import { Exam, ExamAdapted, Question } from '../../interfaces/exams';
+import {
+  Exam,
+  ExamAdapted,
+  ExamResult,
+  Question,
+} from '../../interfaces/exams';
 import { ExamService } from './../../services/exam.service';
 import { MessageService } from 'primeng/api';
 import { ButtonComponent } from '../../../shared/components/ui/button/button.component';
 import { SkeletonModule } from 'primeng/skeleton';
 import { DialogComponent } from '../../../shared/components/ui/dialog/dialog.component';
+import { ExamDialogComponent } from '../../../shared/components/ui/exam-dialog/exam-dialog.component';
+import { ResultDialogComponent } from '../../../shared/components/ui/result-dialog/result-dialog.component';
 
 @Component({
   selector: 'app-start-quiz',
   standalone: true,
-  imports: [DataViewModule, ButtonComponent, SkeletonModule, DialogComponent],
+  imports: [
+    DataViewModule,
+    ButtonComponent,
+    SkeletonModule,
+    DialogComponent,
+    ExamDialogComponent,
+    ResultDialogComponent,
+  ],
   templateUrl: './start-quiz.component.html',
   styleUrl: './start-quiz.component.scss',
 })
@@ -21,6 +35,11 @@ export class StartQuizComponent implements OnInit {
   ) {}
   exams = signal<Exam[]>([]);
   examQuestions = signal<Question[]>([]);
+  showExamModal = signal<boolean>(false);
+  showExamResultModal = signal<boolean>(false);
+  questionNumber = signal<number>(0);
+  examResult = signal<ExamResult[]>([]);
+
   @Input({ required: true }) subjectId: string = '';
   currentPage: number = 1;
   limit: number = 20;
@@ -71,5 +90,32 @@ export class StartQuizComponent implements OnInit {
     }
   }
 
-  startExam(action: string) {}
+  startExam(action: string) {
+    if (action === 'Start') {
+      this.showConfirmationDialog.set(false);
+      if (this.examQuestions().length > 0) {
+        this.showExamModal.set(true);
+      } else {
+        this._messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No questions found',
+        });
+      }
+    }
+  }
+
+  sendQuestion(action: string) {
+    if (action === 'Next') {
+      this.questionNumber.update((value) => value + 1);
+    } else if (action === 'Back') {
+      this.questionNumber.update((value) => value - 1);
+    }
+  }
+
+  showExamResults(results: ExamResult[]) {
+    this.showExamModal.set(false);
+    this.showExamResultModal.set(true);
+    this.examResult.set(results);
+  }
 }
