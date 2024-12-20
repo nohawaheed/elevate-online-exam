34abf81d75ceac74824531077dfb71d1b924fd88
nohawaheed <nohawaheed.1991@gmail.com>
2024-12-21 00:33:54 +0200

    update quiz history

--- src/app/feature/pages/quiz-history/quiz-history.component.ts
+++ src/app/feature/pages/quiz-history/quiz-history.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { DataViewComponent } from '../../../shared/components/ui/data-view/data-view.component';
+import { ExamService } from '../../services/exam.service';
+import { Exam, History } from '../../interfaces/exams';
+import { switchMap } from 'rxjs';
+import { AnswersDialogComponent } from '../../../shared/components/ui/answers-dialog/answers-dialog.component';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-quiz-history',
   standalone: true,
-  imports: [],
+  imports: [DataViewComponent, AnswersDialogComponent, ProgressSpinnerModule],
   templateUrl: './quiz-history.component.html',
-  styleUrl: './quiz-history.component.scss'
+  styleUrl: './quiz-history.component.scss',
 })
-export class QuizHistoryComponent {
+export class QuizHistoryComponent implements OnInit {
+  constructor(private _examService: ExamService) {}
+  history = signal<History[]>([]);
+  examInfo = signal<Exam>({} as Exam);
+  showAnswersModal = signal<boolean>(false);
+  loading = signal<boolean>(false);
 
+  ngOnInit(): void {
+    this.loading.set(true);
+    this._examService
+      .getExamHistory()
+      .pipe(
+        switchMap((exam) => {
+          this.history.set([exam.history]);
+          return this._examService.getExamById(exam.history.QID.exam);
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          if (res.message === 'success') {
+            this.loading.set(false);
+            this.examInfo.set(res.exam);
+          }
+        },
+      });
+  }
+
+  showAnswers(showAnswersDialog: boolean) {
+    this.showAnswersModal.set(showAnswersDialog);
+  }
 }
